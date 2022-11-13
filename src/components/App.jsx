a8ea5863e9b6f646c30c52b9bf7cd140bdaf0d39
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-11-13 14:12:30 +0100

    fix route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,8 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
